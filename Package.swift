19d4b73e8bb02d2abd93fbeaeab7b6200572a0bd
Nikita <me22anine@yandex.ru>
2022-12-29 14:51:16 +0100

    Added Apollo+Combine extension

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "ApolloCombine",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v12),
+        .watchOS(.v5),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +20,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ApolloCombine",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ApolloAPI", package: "apollo-ios"),
+                .product(name: "Apollo", package: "apollo-ios"),
+            ]),
         .testTarget(
             name: "ApolloCombineTests",
             dependencies: ["ApolloCombine"]),
--- Sources/ApolloCombine/ApolloCombine.swift
+++ Sources/ApolloCombine/ApolloCombine.swift
@@ -1,6 +1,100 @@
-public struct ApolloCombine {
-    public private(set) var text = "Hello, World!"
+import ApolloAPI
+import Apollo
+import Combine
 
-    public init() {
+public protocol ApolloCombine {
+    
+    func fetch<Query: GraphQLQuery, T>(
+        _ query: Query,
+        cachePolicy: CachePolicy,
+        data: @escaping (Query.Data?)->T?)
+    -> Future<T?, Error>
+    
+    func perform<Mutation: GraphQLMutation, T>(
+        _ mutation: Mutation,
+        publishResultToStore: Bool,
+        data: @escaping (Mutation.Data?)->T?)
+    -> Future<T?, Error>
+    
+    func upload<Operation: GraphQLOperation, T>(
+        operation: Operation,
+        files: [GraphQLFile],
+        data: @escaping (Operation.Data?)->T?
+    ) -> Future<T?, Error>
+    
+    func subscribe<Subscription: GraphQLSubscription, T>(
+        _ subscription: Subscription,
+        data: @escaping (Subscription.Data?)->T?
+    ) -> Future<T?, Error>
+}
+
+extension ApolloClient: ApolloCombine {
+    
+    public func fetch<Query, T>(_ query: Query, cachePolicy: Apollo.CachePolicy, data: @escaping (Query.Data?) -> T?) -> Future<T?, Error> where Query : ApolloAPI.GraphQLQuery {
+        Future { [unowned self] promise in
+            self.fetch(query: query, cachePolicy: cachePolicy) { queryResult in
+                switch queryResult {
+                case .success(let graphQLResult):
+                    guard let error = graphQLResult.errors?.first else {
+                        promise(.success(data(graphQLResult.data)))
+                        return
+                    }
+                    promise(.failure(error))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+    
+    public func perform<Mutation, T>(_ mutation: Mutation, publishResultToStore: Bool, data: @escaping (Mutation.Data?) -> T?) -> Future<T?, Error> where Mutation : ApolloAPI.GraphQLMutation {
+        Future { [unowned self] promise in
+            self.perform(mutation: mutation, publishResultToStore: publishResultToStore) { queryResult in
+                switch queryResult {
+                case .success(let graphQLResult):
+                    guard let error = graphQLResult.errors?.first else {
+                        promise(.success(data(graphQLResult.data)))
+                        return
+                    }
+                    promise(.failure(error))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+    
+    public func upload<Operation, T>(operation: Operation, files: [Apollo.GraphQLFile], data: @escaping (Operation.Data?) -> T?) -> Future<T?, Error> where Operation : ApolloAPI.GraphQLOperation {
+        Future { [unowned self] promise in
+            self.upload(operation: operation, files: files) { uploadResult in
+                switch uploadResult {
+                case .success(let graphQLResult):
+                    guard let error = graphQLResult.errors?.first else {
+                        promise(.success(data(graphQLResult.data)))
+                        return
+                    }
+                    promise(.failure(error))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+    
+    public func subscribe<Subscription, T>(_ subscription: Subscription, data: @escaping (Subscription.Data?) -> T?) -> Future<T?, Error> where Subscription : ApolloAPI.GraphQLSubscription {
+        Future { [unowned self] promise in
+            self.subscribe(subscription: subscription) { result in
+                switch result {
+                case .success(let graphQLResult):
+                    guard let error = graphQLResult.errors?.first else {
+                        promise(.success(data(graphQLResult.data)))
+                        return
+                    }
+                    promise(.failure(error))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
+            }
+        }
     }
 }
